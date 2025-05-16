6274b5e5c3ba7d1eed2604c9472e8c6b57b4a38b
DJCC006 <david.carcamo2006@gmail.com>
2025-05-16 13:16:26 -0600

    Creaccion de proyecto

---
+++ Santos_Carcamo_Daniel_David_Estructura/src/santos_carcamo_daniel_david_estructura/Santos_Carcamo_Daniel_David_Estructura.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package santos_carcamo_daniel_david_estructura;
+
+/**
+ *
+ * @author David
+ */
+public class Santos_Carcamo_Daniel_David_Estructura {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
