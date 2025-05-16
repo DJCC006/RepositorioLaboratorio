6c1dd6bc68f7ed8feeb3ccae023d1a1700ff150e
DJCC006 <david.carcamo2006@gmail.com>
2025-05-16 13:35:55 -0600

    Update 1

--- Santos_Carcamo_Daniel_David_Estructura/src/santos_carcamo_daniel_david_estructura/Santos_Carcamo_Daniel_David_Estructura.java
+++ Santos_Carcamo_Daniel_David_Estructura/src/santos_carcamo_daniel_david_estructura/Santos_Carcamo_Daniel_David_Estructura.java
@@ -3,18 +3,40 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package santos_carcamo_daniel_david_estructura;
-
+ import java.util.Scanner;
 /**
  *
  * @author David
  */
 public class Santos_Carcamo_Daniel_David_Estructura {
-
+   
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Scanner lea= new Scanner(System.in);
+        lea.useDelimiter("\n");
+        
+        
+        //Parte de cifrado ceasar
+        
+        
+        //Recurso de referencia
+        String abecedario= "ABCDFGHIJKLMNÑOPQRSTUVWXYZ";
+        String texto_codificado=" ";
+        
+       
+        System.out.println("Ingrese una frase: ");
+        String frase=lea.next().toUpperCase();
+        System.out.println(frase);
+        
+        
+        
+        
+        
+        
     }
     
 }
