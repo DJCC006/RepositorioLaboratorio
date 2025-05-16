b9e836fdd898c6867a05b2b1ff2e2427be8b0f07
DJCC006 <david.carcamo2006@gmail.com>
2025-05-16 13:49:49 -0600

    Update 2

--- Santos_Carcamo_Daniel_David_Estructura/src/santos_carcamo_daniel_david_estructura/Santos_Carcamo_Daniel_David_Estructura.java
+++ Santos_Carcamo_Daniel_David_Estructura/src/santos_carcamo_daniel_david_estructura/Santos_Carcamo_Daniel_David_Estructura.java
@@ -25,19 +25,37 @@ public class Santos_Carcamo_Daniel_David_Estructura {
         
         //Recurso de referencia
         String abecedario= "ABCDFGHIJKLMNÑOPQRSTUVWXYZ";
-        String texto_codificado=" ";
+        String texto_cifrado=" ";
         
        
+        //Ingreso de frase inicial
         System.out.println("Ingrese una frase: ");
         String frase=lea.next().toUpperCase();
         System.out.println(frase);
         
+        //Ingreso de indice de salto
+        System.out.println("Ingrese el indice de salto: ");
+        int indexSalto= lea.nextInt();
+        
         int fraselength=frase.length();
         
+        //Ciclo recorredor de frase
         int i;
         for(i=0; i<=(fraselength-1); i++){
+            char letra=frase.charAt(i);
             
-        }
+            //Variable que referencia la posicion en el abecedario
+            int pos= abecedario.indexOf(letra);
+            
+            //pos==-1 en caso que no coincida con ninguna de las letras del abecedario
+            if(pos==-1){
+                texto_cifrado+=letra;
+            }else{
+                texto_cifrado+=abecedario.charAt((pos+indexSalto)%fraselength);
+            }
+            
+        }//fin for
+        System.out.println("Texto Cifrado: "+texto_cifrado);
         
         
         
