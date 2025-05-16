9be578bed87b301279e267eaadc27781bd9cb33c
tusderechos <hidrosoluciones61@gmail.com>
2025-05-16 15:10:55 -0600

    Filtrado de Palabras

--- Santos_Carcamo_Daniel_David_Estructura/src/santos_carcamo_daniel_david_estructura/Santos_Carcamo_Daniel_David_Estructura.java
+++ Santos_Carcamo_Daniel_David_Estructura/src/santos_carcamo_daniel_david_estructura/Santos_Carcamo_Daniel_David_Estructura.java
@@ -16,52 +16,86 @@ public class Santos_Carcamo_Daniel_David_Estructura {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        int Opcion;
+        
         Scanner lea= new Scanner(System.in);
         lea.useDelimiter("\n");
         
-        
-        //Parte de cifrado ceasar
-        
-        
-        //Recurso de referencia
-        String abecedario= "ABCDFGHIJKLMNÑOPQRSTUVWXYZ";
-        String texto_cifrado=" ";
-        
-       
-        //Ingreso de frase inicial
-        System.out.println("Ingrese una frase: ");
-        String frase=lea.next().toUpperCase();
-        System.out.println(frase);
-        
-        //Ingreso de indice de salto
-        System.out.println("Ingrese el indice de salto: ");
-        int indexSalto= lea.nextInt();
-        
-        int fraselength=frase.length();
-        
-        //Ciclo recorredor de frase
-        int i;
-        for(i=0; i<=(fraselength-1); i++){
-            char letra=frase.charAt(i);
+        do {            
+            System.out.println("-----------------------------");
+            System.out.println("|       Menu Principal      |");
+            System.out.println("-----------------------------");
+            System.out.println("|Opciones disponibles:      |");
+            System.out.println("| 1. Cifrado Cesar          |");
+            System.out.println("| 2. Filtrado de Palabras   |");
+            System.out.println("| 3. Codigo Enigma          |");
+            System.out.println("-----------------------------");
+            System.out.println("\r                                                                          ");
+            System.out.print("Bienvenido al menu principal, ingrese una opcion para poder hacer: ");
+            Opcion = lea.nextInt();
+            System.out.println("\r                                                                          ");
             
-            //Variable que referencia la posicion en el abecedario
-            int pos= abecedario.indexOf(letra);
-            
-            //pos==-1 en caso que no coincida con ninguna de las letras del abecedario
-            if(pos==-1){
-                texto_cifrado+=letra;
-            }else{
-                texto_cifrado+=abecedario.charAt((pos+indexSalto)%fraselength);
+            switch(Opcion) {
+                case 1:
+                    //Parte de Cifrado Cesar
+                    System.out.println("cifrado cesar");
+                case 2:
+                    //Parte de Filtrado de Palabras
+                    System.out.println("Ingrese una frase: ");
+                    String Frase = lea.next();
+                    
+                    System.out.println("Ingrese un limite de caracteres: ");
+                    int Limite = lea.nextInt();
+                    
+                    if (Limite < 0) {
+                        System.out.println("El limite de caracteres tiene que ser mayor a 0, ingrese un limite mayor a 0");
+                        break;
+                    }
+                    
+                    Frase = Frase + " "; //aqui solo se le suma un espacio a la frase para que se lea bien la ultima palabra
+                    int Inicio = 0; //inicio de la frase
+                    int Longitud = Frase.length();
+                    
+                    System.out.println("Lista de Palabras: ");
+                    for (int i = 0; i < Longitud; i++) {
+                        if (Frase.charAt(i) == ' ') {
+                            String Palabra = Frase.substring(Inicio, i);
+                            String Palabralimpia = Palabra.replaceAll("\\p{Punct}", "");
+                            System.out.println(" " + Palabralimpia);
+                            Inicio = i + 1;
+                        }
+                    }
+                    
+                    //for para recorrer la frase una y otra vez para encontrar palabras que sean mayores al limite ingresado
+                    System.out.println("Palabras con caracteres mayor al limite: " + Limite);
+                    Inicio = 0;
+                    
+                    for (int i = 0; i < Longitud; i++) {
+                        if (Frase.charAt(i) == ' ') {
+                            String Palabra = Frase.substring(Inicio, i);
+                            String Palabralimpia = Palabra.replaceAll("\\p{Punct}", ""); //aqui se elimina cualquier pleca, coma, punto, signo de pregunta/exlamacion 
+                            if (Palabralimpia.length() >= Limite) {
+                                System.out.println(" " + Palabralimpia);
+                            }
+                            Inicio = i + 1;
+                        }
+                    }
+                    
+                    break;
+                    
+                case 3:
+                    //Parte de Codigo Enigma
+                    System.out.println("codigo enigma");
+                    
+                    break;
+                    
+                default:
+                    System.out.println("Esa opcion no es valida, por favor ingrese una opcion que sea valida.");
+                    break;
             }
-            
-        }//fin for
-        System.out.println("Texto Cifrado: "+texto_cifrado);
-        
-        
-        
-        
-        
-        
+                
+        } while (Opcion >= 3);
+    
     }
     
 }
